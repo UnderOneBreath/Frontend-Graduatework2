fbdab7d4d4ffbf8a3f47f00006539a3d8d54e994
UnderOneBreath <63227827+UnderOneBreath@users.noreply.github.com>
2026-02-26 01:11:02 +0900

    minimal Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,13 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 export default function Header() {
-    return (
-        <>
-            <div>
-                <Button variant="ghost">Ghost</Button>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div className="w-full h-16 border-b border-gray-200 justify-center items-center flex">
+				<div className="h-12 w-36 justify-center items-center flex">
+					<Button variant="ghost">Войти</Button>
+				</div>
+			</div>
+		</>
+	);
 }
