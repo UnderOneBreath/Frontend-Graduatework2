2806d51282eca7d878a59e6e25b08f45c4aef336
UnderOneBreath <memories53@mail.ru>
2026-02-25 14:51:54 +0300

    its work, ui kit and tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "tailwindcss";
 import '@/index.css'
+import Header from "@/components/header";
 import * as Pages from "@/pages";
 
 function App() {
   return (
     <>
-
+      <Header></Header>
     </>
   )
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+export default function Header() {
+    return (
+        <>
+            <div>
+                <Button variant="ghost">Ghost</Button>
+            </div>
+        </>
+    )
+}
